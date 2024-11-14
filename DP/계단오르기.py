c1445ebed5d4e2f08c95857c7cf7f9c9592f4014
namseokkim <skatjr30@gmail.com>
2024-11-14 10:26:14 +0900

    계단오르기 완료

--- DP/계단오르기.py
+++ DP/계단오르기.py
@@ -1,15 +1,23 @@
 n = int(input())
 
-array = []
+array = [0]
 dp = dict()
 
-for i in range(n):
+for i in range(1, n + 1):
     value = int(input())
     array.append(value)
 
+    if i == 1:
+        dp[i] = value
+    elif i == 2:
+        dp[i] = dp[i - 1] + value
+    elif i == 3:
+        dp[i] = max(dp[i - 2] + value, array[i - 1] + value)
 
-def answer(index):
-    
-    array[index]
 
-    dp[index] = max(dp[index - 1], dp[index - 2] + dp[index - 3]) + array[index]
+if n > 3:
+    for i in range(4, n + 1):
+        dp[i] = max(dp[i - 3] + array[i - 1], dp[i - 2]) + array[i]
+
+
+print(dp[n])
