9731ee28a0905d42ccb0f700c0f4763ff8150751
namseokkim <skatjr30@gmail.com>
2024-11-15 19:54:25 +0900

    평범한 배낭

--- DP/평범한배낭.py
+++ DP/평범한배낭.py
@@ -1,17 +1,19 @@
-     # n 개의 물건
+# n 개의 물건
 # w : 무게, v : 가치
 # k : 최대무개
-
-
 n, k = map(int, input().split())
-
 arrays = []
-answers = []
+dp = dict()
+
+for i in range(k + 1):
+    dp[i] = 0
 
 for i in range(n):
-    w, k = map(int, input().split())
-    arrays.append((w, k))
+    w, v = map(int, input().split())
+    arrays.append((w, v))
 
-arrays.sort()
+for w, v in arrays:
+    for j in range(k, w - 1, -1):
+        dp[j] = max(dp[j], dp[j - w] + v)
 
-print(arrays)
+print(dp[k])
